9e7d54598423bab3d70da9b30c3a2aa2455dfc87
seumbae <ohcm0013@gmail.com>
2023-10-11 01:12:50 +0900

    Create Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Header = () => {
+    return <Bar />
+}
+
+export default Header;
+
+const Bar = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: #21487f;
+`
