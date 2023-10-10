f41e43ed1d56e4535b5ad7e3a4c3c67dee6185e1
seumbae <ohcm0013@gmail.com>
2023-10-11 01:10:23 +0900

    organize Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Main from "./containers/Main";
+import Layout from "./containers/common/Layout";
+import Notice from "./containers/notice/Notice";
+import SampleMain from "./containers/sample/sampleMain";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route element={<Layout />}>
+        <Route path="/notice/:id" element={<Notice />} />
+        <Route path="/sample" element={<SampleMain />} />
+      </Route>
+    </Routes>
   );
 }
 
