6623fb0eee1613f4b1b4ff813327430ecfe563fb
seumbae <ohcm0013@gmail.com>
2023-10-12 00:33:20 +0900

    create: example page (not completed)

--- src/containers/sample/sampleMain.jsx
+++ src/containers/sample/sampleMain.jsx
@@ -3,13 +3,17 @@ import { useLocation, useNavigate } from "react-router-dom";
 import Container from "../../components/Container";
 import Title from "../../components/Title";
 import TimerContainer from "../common/TimerContainer";
+import SelectionExam from "../../components/SelectionExam";
+import ExamContent from "../../components/samplePage/ExamContent";
+import { ContentContainer, ContentsWrapper, FlexBoxSBwithBorder } from "../../components/Common";
 
 const SampleMain = () => {
     const location = useLocation();
     const navigate = useNavigate();
 
     const [time, setTime] = useState(localStorage.getItem('timeLeft') || 2);
-
+    
+    const contents = ["나는 정서적으로 안정되어 있다.", "별로다완전"]
 	useEffect(() => {
 		if (location.state === null || location.state.flag === false) {
             // navigate("/");
@@ -22,12 +26,20 @@ const SampleMain = () => {
                 <Title>예시문제</Title>
                 <TimerContainer time={time} setTime={setTime}/>
             </Container>
-            <div>
-                hello
-            </div>
-            <div>
-                hello
-            </div>
+            <SelectionExam />
+            <ContentsWrapper>
+                {contents.map((content, index) => {
+                    return (
+                        <FlexBoxSBwithBorder>
+                            <ContentContainer key={index}>
+                                {index+1}. {content}
+                            </ContentContainer>
+                            <SelectionExam />
+                        </FlexBoxSBwithBorder>
+                    )
+                    })}        
+            </ContentsWrapper>
+            {/* <ExamContent /> */}
         </Container>
     )
 }
