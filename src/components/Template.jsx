65ea7e18d75f45d32e6a255cf373936b7bddefe2
seumbae <ohcm0013@gmail.com>
2023-10-21 22:32:07 +0900

    add media query

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -7,6 +7,17 @@ const Template = ({ children }) => {
 export default Template;
 
 const Container = styled.div`
-    padding: 80px 150px 0 150px;
     height: 80%;
+
+    @media (min-width: 1024px){
+        padding: 5rem 8rem 0 8rem;
+    }
+
+    @media (min-width: 768px) and (max-width: 1023px) {
+        padding: 5rem 8rem 0 8rem;
+    }
+
+    @media (max-width: 767px) {
+        padding: 3rem 3rem 0 3rem;
+    }
 `;
