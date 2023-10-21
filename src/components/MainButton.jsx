8c8369d79ce9c4d3de3c727626de0f0506833f5d
seumbae <ohcm0013@gmail.com>
2023-10-21 22:31:14 +0900

    add media query

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -2,8 +2,6 @@ import styled from "styled-components";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Btn = styled.div`
-    width: 120px;
-    height: 30px;
     background-color: #21487f;
     color: #ffffff;
     border: none;
@@ -12,6 +10,23 @@ const Btn = styled.div`
     cursor: pointer;
     display: flex;
     align-items: center;
+    font-weight: bold;
+
+    @media (min-width: 1024px){
+        width: 7.5rem;
+        height: 1.8rem;
+    }
+    @media (min-width: 768px) and (max-width: 1023px) {
+        width: 6rem;
+        height: 1.6rem;
+    }
+
+    @media (max-width: 767px) {
+        width: 5rem;
+        height: 1.5rem;
+        font-size: 0.9rem;
+    }
+    
 `
 
 const ArrowIcon = styled(ArrowForwardIcon)`
