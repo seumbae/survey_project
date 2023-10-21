4283cef36f26466d8f479b5af8e9ed1f39790d16
seumbae <ohcm0013@gmail.com>
2023-10-21 22:30:40 +0900

    apply styled-components

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,5 +1,15 @@
-const Email = ({onChange}) => {
-    return <input type="email" placeholder="이메일을 입력해주세요." size={45} onChange={onChange}/>
-}
+import styled from "styled-components";
+
+const Email = ({ onChange }) => {
+	return (
+        <Input onChange={onChange} />
+	);
+};
 
 export default Email;
+
+const Input = styled.input.attrs({type: 'email', placeholder:"abc@gmail.com", size:"40"})`
+    height: 1.3rem;
+    padding: 0.3rem;
+    border: 1px solid #888888;
+`
