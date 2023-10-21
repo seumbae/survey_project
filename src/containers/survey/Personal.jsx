f3f6b092ac828986ee1ff2554e983dbbb8079f6d
seumbae <ohcm0013@gmail.com>
2023-10-21 22:38:03 +0900

    add get data logic, send data to server logic

--- src/containers/survey/Personal.jsx
+++ src/containers/survey/Personal.jsx
@@ -12,18 +12,20 @@ import MainButton from "../../components/MainButton";
 import SelectionExam from "../../components/SelectionExam";
 import Title from "../../components/Title";
 import TimerContainer from "../common/TimerContainer";
-import { question } from "../../static/example";
 import PrevButton from "../../components/PrevButton";
+import { getPersonalQuestions, postAnswer } from "../../services/sevice";
+import Contents from "../../components/Contents";
 
 const Personal = () => {
 	const location = useLocation();
 	const navigate = useNavigate();
 
-	const [time, setTime] = useState(localStorage.getItem("timeLeft") || 2);
+	const [userId, setUserId] = useState(null);
+	const [time, setTime] = useState(localStorage.getItem("timeLeft") || 210);
 	const [checked, setChecked] = useState({});
 	const [current, setCurrent] = useState(1);
 
-	const contents = question;
+	const [contents, setContents] = useState([]);
 	const delimiter = 10;
 
 	useEffect(() => {
@@ -31,63 +33,94 @@ const Personal = () => {
 			navigate("/");
 		}
 
-		const initialChecked = Object.fromEntries(
-			Array.from({ length: contents["questions"].length }, (_, i) => [i, -1])
-		);
-		setChecked(initialChecked);
+		getPersonalQuestions(1)
+			.then((res) => {
+				if (res.status === 200) {
+					setContents(res.data);
+					setUserId(localStorage.getItem("userId"));
+					setChecked(
+						Object.fromEntries(
+							Array.from({ length: res.data.length }, (_, i) => [i + 1, -1])
+						)
+					);
+				}
+			})
+			.catch((err) => {
+				alert("문항 불러오기에 실패했습니다. 관리자에게 문의하십시오.");
+				navigate("/");
+			});
 	}, []);
 
 	const onCheckHandler = (e) => {
 		e.preventDefault();
-
 		if (checked[e.target.parentElement.id] === Number(e.target.id)) {
 			setChecked({ ...checked, [e.target.parentElement.id]: -1 });
-		} else
+		} else {
 			setChecked({
 				...checked,
 				[e.target.parentElement.id]: Number(e.target.id),
 			});
+		}
 	};
 
-    const onPrevHandler = (e) => {
-        if(current === 1){
-            alert("이전 문항이 존재하지 않습니다.");
-        }
-        else{
-            setCurrent(current - 1);
-        }
-    }
+	const onPrevHandler = (e) => {
+		if (current === 1) {
+			alert("이전 문항이 존재하지 않습니다.");
+		} else {
+			setCurrent(current - 1);
+		}
+	};
 
 	const onNextHandler = (e) => {
-        const start = (current - 1) * delimiter;
+		const start = (current - 1) * delimiter;
 		const end = start + delimiter;
-        
-        // if(Object.values(checked).slice(start, end).some(item => item === -1)){
-        //     alert("모든 문항에 답해주세요.");
-        // }
-		// else 
-        if (contents["questions"].length < delimiter * current) {
+
+		if (
+			Object.values(checked)
+				.slice(start, end)
+				.some((item) => item === -1)
+		) {
+			alert("모든 문항에 답해주세요.");
+		} else if (contents.length < delimiter * current) {
+			if (localStorage.getItem("timeLeft") !== undefined) {
+				localStorage.removeItem("timeLeft");
+			}
+			onHandleComplete();
+			alert("감사합니다. 개인 성향 관련 검사를 종료합니다.");
 			navigate(`/survey/1`, { state: { flag: true, index: 1 } });
-            //TODO: 데이터 보내기
-		} else{
+		} else {
 			setCurrent(current + 1);
 		}
+	};
+
+	const onHandleComplete = () => {
+		const data = [];
+		Object.entries(checked).forEach(([key, value]) => {
+			data.push({
+				Score: value,
+				FormId: key,
+				UserId: userId,
+				FormType: 1,
+			});
+		});
+
+		postAnswer(data);
 	};
 
 	const currentQuestions = () => {
 		const start = (current - 1) * delimiter;
 		const end = start + delimiter;
 
-		return contents["questions"].slice(start, end).map((content, index) => {
+		return contents.slice(start, end).map((content, index) => {
 			return (
 				<FlexBoxSBwithBorder>
-					<ContentContainer key={start + index}>
-						{start + index + 1}. {content}
+					<ContentContainer key={content.id}>
+						{content.id}. {content.title}
 					</ContentContainer>
 					<AnswerBtn
 						onClick={onCheckHandler}
-						index={start+index}
-						clicked={checked[start + index]}
+						index={content.id}
+						clicked={checked[content.id]}
 					/>
 				</FlexBoxSBwithBorder>
 			);
@@ -98,22 +131,31 @@ const Personal = () => {
 		<Container height="inherit" display="flex" flexDirection="column" gap="30">
 			<Container display="flex" justifyContent="space-between">
 				<Title>개인 성향 관련 검사</Title>
-				<TimerContainer time={time} setTime={setTime} />
+				<TimerContainer
+					time={time}
+					setTime={setTime}
+					formType={0}
+					onComplete={onHandleComplete}
+				/>
 			</Container>
 			<SelectionExam border={true} />
 			<ContentsWrapper>{currentQuestions()}</ContentsWrapper>
-			<FlexContainer>
-                <Container>
-                    <PrevButton content="이전" onClick={onPrevHandler}/>
-                </Container>
-				<Container>
-					<MainButton
-						content="다음"
-						flag={true}
-						onClick={(e) => onNextHandler(2, e)}
-					/>
+			<Container display="flex" flexDirection="column" gap="10">
+				<FlexContainer>
+					<Container>
+						<PrevButton content="이전" onClick={onPrevHandler} />
+					</Container>
+					<Container alignSelf="end">
+						<MainButton content="다음" flag={true} onClick={onNextHandler} />
+					</Container>
+				</FlexContainer>
+				<Container alignSelf="end">
+					<Contents color="#888888">
+						모든 문항에 응답하셔야 다음 페이지로 넘어갈 수 있으며 시간 종료시
+						설문은 종료됩니다.
+					</Contents>
 				</Container>
-			</FlexContainer>
+			</Container>
 		</Container>
 	);
 };
