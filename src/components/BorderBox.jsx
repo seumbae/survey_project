910a9e685005e8effbaeafb507acba3482aa4e0b
seumbae <ohcm0013@gmail.com>
2023-10-21 22:29:22 +0900

    add media query

--- src/components/BorderBox.jsx
+++ src/components/BorderBox.jsx
@@ -14,4 +14,8 @@ const Container = styled.div`
     border: 5px solid #21487f;
     padding: 40px 40px;
     width: -webkit-fill-available;
+
+    @media (max-width: 767px) {
+        padding: 2rem 2rem;
+    }
 `
