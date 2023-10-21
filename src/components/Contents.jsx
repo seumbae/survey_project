2b86b47f87d275e853d9839504fdffe3b0ba2076
seumbae <ohcm0013@gmail.com>
2023-10-21 22:30:08 +0900

    add media query

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,12 +1,25 @@
 import styled from "styled-components"
 
 const Contents = (props) => {
-    return <Content fontSize={props.fontSize} color={props.color}>{props.children}</Content>
+    return <Content width={props.width} marginRight={props.marginRight} bold={props.bold} color={props.color}>{props.children}</Content>
 }
 
 export default Contents;
 
 const Content = styled.div`
-    font-size: ${(props) => (props.fontSize ? props.fontSize : "16px")};
-    color: ${(props) => (props.color ? props.color : "#000000")}
+    @media (min-width: 1024px){
+        font-size:1.3rem;
+    }
+
+    @media (min-width: 768px) and (max-width: 1023px) {
+        font-size: 1.3rem;
+    }
+
+    @media (max-width: 767px) {
+        font-size: 1rem;
+    }
+    width: ${(props) => (props.width ? props.width : "")};
+    color: ${(props) => (props.color ? props.color : "#000000")};
+    font-weight: ${(props) => (props.bold ? "bold" : "")};
+    margin-right: ${(props) => (props.marginRight ? props.marginRight : "")};
 `
